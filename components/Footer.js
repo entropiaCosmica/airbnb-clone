7942dfe38df2565ad5ffe3bdafa7199077f1234d
gelatina <santiagozapatabohorquez@hotmail.com>
2021-11-18 12:30:23 -0500

    Added main screen

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>COVID-19</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accesibility</p>
+        <p>Support afgan refugees</p>
+        <p>Celebrate diversity</p>
+        <p>We fight against discrimination</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Share your space</p>
+        <p>AirCover: protection for hosts</p>
+        <p>Resources</p>
+        <p>Visit the forum</p>
+        <p>How to responsibly bring host services</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Careers</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
